e24e111b4be3151376f76754f1c3402ca0eb5a88
Oscar R.C <oscarrc.web@gmail.com>
2020-08-24 18:07:31 +0200

    [MOD] Document FanartTV repository

--- src/repositories/fanarttv.repository.js
+++ src/repositories/fanarttv.repository.js
@@ -1,12 +1,36 @@
 const {FANART_URL, FANART_KEY} = require('../../config');
 const axios = require('axios');
 
+/**
+ * FanartTV repository. 
+ * Interfaces with FanartTV API to get images
+ * 
+ * 
+ * @memberof module:repositories
+ * @class LastFmRepository
+ * @param {String} url - Url of the API
+ * @param {String}  key - FanartTV API Key
+ */
+
 class FanartTvRepository{
-    constructor(FANART_URL, FANART_KEY){
-        this.url = FANART_URL;
-        this.key = FANART_KEY
+    constructor(url, key){
+        this.url = url;
+        this.key = key
     }
 
+    /**
+     * Gets image for given format (method) and mbid
+     * 
+     * @function getImage
+     * @memberof module:repositories.FanartTvRepository
+     * @this module:repositories.FanartTvRepository
+     * @param {String} mbid - MBID of the artist or album
+     * @param {String} method - Method to call, regarding what info we want to get
+     * @returns {Object} - Object containing the matching images
+     * @throws {Object} - Object detailing the error
+     * @instance
+     * @async
+     */
     async getImage(mbid, method){
         const url = FANART_URL + ( method !== 'artist' ? '/' + method : '/') + mbid + '?api_key=' + this.key;
 
