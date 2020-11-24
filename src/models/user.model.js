60bda74550ff6501997dc530cdfd82c9bed383b7
Oscar R.C <oscarrc.web@gmail.com>
2020-11-24 12:21:24 +0100

    [MOD] Default public profile value

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -127,7 +127,7 @@ const User = new Schema({
     settings:{
         publicProfile:{
             type: Boolean,
-            default: false,
+            default: true,
             required: true
         },
         publicFavorites:{
