c89a7bb9c604032c573428c771b56e7dc320aac7
Oscar <oscarrc.web@gmail.com>
2021-02-01 14:21:26 +0100

    [MOD] Config registration open as boolean

--- config/index.js
+++ config/index.js
@@ -26,5 +26,5 @@ module.exports = {
     VERSION: process.env.VERSION,  
     WEB_CONCURRENCY: process.env.WEB_CONCURRENCY,
     WEB_URL: process.env.WEB_URL,
-    REGISTRATION_OPEN: process.env.REGISTRATION_OPEN
+    REGISTRATION_OPEN: process.env.REGISTRATION_OPEN === 'true'
 };
