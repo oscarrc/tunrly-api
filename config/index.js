7b8a72e3d54e1874091324319d51fd52e5c9d03f
Oscar <oscarrc.web@gmail.com>
2021-04-22 11:00:24 +0200

    [DEL] Donations env variable, it's unused

--- config/index.js
+++ config/index.js
@@ -26,6 +26,5 @@ module.exports = {
     VERSION: process.env.VERSION,  
     WEB_CONCURRENCY: process.env.WEB_CONCURRENCY,
     WEB_URL: process.env.WEB_URL,
-    INVITE: process.env.INVITE,
-    DONATIONS_TARGET: process.env.DONATIONS_TARGET
+    INVITE: process.env.INVITE
 };
